ee5ef938ec15f26e22f3259c75b5316f3ccb9ff6
Fadhol <muhfadhol43@gmail.com>
2026-05-09 21:39:47 +0700

    update ui background

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
   const location = useLocation()
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-zinc-950 via-black to-zinc-900 text-white">
+    <div className="bg-black min-h-screen text-white">
 
       <Navbar />
 
